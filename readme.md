0214180ebd13aa395d8184807fe83f49c200ef0a
Adson Cicilioti <eu@adsonagencia.com>
2021-09-15 23:05:22 -0300

    Init

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Style Chatbot Multi360
+
+Customization of Multi360's Chatbot for Flora Nativa do Brasil client.
---
+++ script.js
@@ -0,0 +1,25 @@
+var chatBtns = document.getElementsByClassName("chatbtn")
+
+console.log(chatBtns)
+
+for (var i = 0; i < chatBtns.length; i++) {
+  chatBtns[i].addEventListener("click", function (e) {
+    e.preventDefault()
+    WBOTopenChat()
+    e.stopPropagation()
+  })
+}
+
+setTimeout(() => {
+  var headerHello = document.querySelector(
+    "#wbot-header .wbot-info .wbot-client"
+  )
+  // console.log(headerHello)
+  headerHello.innerHTML = "👋 Olá! Eu sou a Flora!"
+}, 1000)
+
+setTimeout(() => {
+  var chatBotBtn = document.getElementById("wbot-open-chat")
+  // console.log(chatBotBtn)
+  chatBotBtn.classList.add("start-msg")
+}, 3000)
